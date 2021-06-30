7b1a08d9f67f4065d0e6176cce3c4015279faaf7
ndeva3199 <ndeva3199@gmail.com>
2021-07-01 01:02:52 +0700

    cm

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -15,8 +15,8 @@ router.get('/google', passport.authenticate('google', {
 }));
 router.get('/google/callback', passport.authenticate('google'),async function (req:any, res) {
   const domain = process.env.NODE_ENV === 'production' ? 'https://serene-ocean-09276.herokuapp.com' : 'http://localhost:3000'
-  res.cookie('accessToken',req.user.generateAuthToken(), {domain: 'serene-ocean-09276.herokuapp.com', sameSite: 'none'})
-  .redirect(domain)
+  res.cookie('accessToken',req.user.generateAuthToken())
+  res.redirect(302,domain)
 });
 
 export default router
